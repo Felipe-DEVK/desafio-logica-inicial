78fdd72a1f19b7c1302d103817937d25e903e7e1
Felipe Dev <felipedeveloper1k@gmail.com>
2025-01-21 12:43:32 +0100

    Ifs e Elses Sendo Inseridos

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("ola mundao")
+const nomeDoHeroi = "Felipe";
+
+let xpDoHeroi = 8750;
+
+
+
+if (xpDoHeroi <= 1000) {
+    console.log("Ferro")
+} else if (xpDoHeroi <= 2000){
+    console.log("Bronze")
+} else if (xpDoHeroi <= 5000){
+    console.log("Prata")
+} else if (xpDoHeroi <= 7000){
+    console.log("Ouro")
+} else if (xpDoHeroi <= 8000){
+    console.log("Platina")
+} else if(xpDoHeroi <= 9000){
+    console.log("Ascendente")
+} else if (xpDoHeroi <= 10000){
+    console.log("Imortal")
+} else {
+    console.log("Radiante")
+}
